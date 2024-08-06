13b83f4a9366d1c2360ed523f59230e40c1ec08c
Poem Maharjan <poemmahrajan4290@gmail.com>
2024-08-06 18:18:31 +0545

    update frontend

---
+++ Frontend/script.js
@@ -0,0 +1,12 @@
+document
+  .getElementById("input_file")
+  .addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        document.getElementById("input_image").src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  });
